5f7006a9d022b8c6428a5978d5c6c9d6081722c4
50ra4 <50ra4.dev.dev@gmail.com>
2024-09-20 20:39:08 +0900

    use commander

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,51 @@
 import express from 'express';
-const app: express.Express = express();
-const port = 3000;
+import { Command } from 'commander';
+import { resolve } from 'node:path';
+import { existsSync, readFileSync } from 'node:fs';
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
-});
+const getOptions = () =>
+  new Command()
+    .requiredOption(
+      '-t, --template [relative template json filepath]',
+      'Specify the template json file path.',
+    )
+    .option(
+      '-p, --port [port number]',
+      'Specify express.js port number.',
+      '3000',
+    )
+    .parse(process.argv)
+    .opts();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+type Target = {
+  endpoint: string;
+  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+  file: string;
+};
+
+const main = () => {
+  const port = +getOptions()['port'];
+  const templatePath: string = resolve(getOptions()['template']);
+
+  if (!existsSync(templatePath)) {
+    throw new Error(`not found template file.(${templatePath})`);
+  }
+
+  const targets: Target[] = JSON.parse(readFileSync(templatePath, 'utf-8'));
+  console.info('==== targets', targets);
+
+  const app: express.Express = express();
+  app.get('/', (_req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+};
+
+try {
+  main();
+} catch (error) {
+  console.error(error);
+}
